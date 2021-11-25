e239b9d0f81786903c1f2ceadce59de997a3660d
AlexandraHockett <87495152+AlexandraHockett@users.noreply.github.com>
2021-11-25 19:31:41 +0000

    Added the GitHub repo

--- src/App.js
+++ src/App.js
@@ -4,6 +4,22 @@ export default function App() {
   return (
     <div className="App">
       <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/alexandra-hockett/"
+          target="_blank"
+        >
+          Alexandra Hockett{" "}
+        </a>
+        and is{" "}
+        <a
+          href="https://github.com/AlexandraHockett/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
     </div>
   );
 }
